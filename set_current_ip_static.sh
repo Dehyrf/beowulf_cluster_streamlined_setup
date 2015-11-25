6c70cbe9974df9aa21b0b40b41c16f8cb49e233c
Dehyrf <Dehyrf@users.noreply.github.com>
2015-11-24 19:56:24 -0600

    Got static setup working and began work on main script

--- set_current_ip_static.sh
+++ set_current_ip_static.sh
@@ -2,20 +2,23 @@
 
 echo Make sure you are connected to ethernet and are running this script as a super user!
 
+read -e -p "Enter the network interface name: " -i "eth0" IFACE
+echo $IFACE
+
 read -e -p "Enter the ip for the gateway: " -i "192.168.10.1" GATEWAY
 echo $GATEWAY
 
 read -e -p "Enter the netmask: " -i "255.255.255.0" MASK
 echo $MASK
 
-IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+IP="$(ifconfig $IFACE | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
 
 HOST=$(hostname)
 
 cd
 sudo touch /etc/network/interfaces 
 sudo echo "
-iface eth0 inet static
+iface $IFACE inet static
      	address "$IP" 
      	netmask "$MASK"
      	gateway "$GATEWAY"" >> /etc/network/interfaces
