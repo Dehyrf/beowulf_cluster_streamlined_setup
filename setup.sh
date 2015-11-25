64de22e0751905169adb7a989fe23a6a5f330d99
Dehyrf <Dehyrf@users.noreply.github.com>
2015-11-25 11:38:58 -0600

    Better organization

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-PASSWORD='password123'
-bash set_ip_volatile.sh 
-bash set_current_ip_static.sh
+echo "Please begin without ethernet plugged in"
+bash ./dependencies/set_ip_volatile.sh 
+echo "Now plug in ethernet. Does the computer show that it is connected? "
+bash ./dependencies/set_current_ip_static.sh
 sudo apt-get update
 sudo --yes install build-essentials openssh-server
 sudo --yes apt-get autoremove && sudo apt-get --yes autoclean
 sudo useradd mpiuser sudo
 sudo passwd mpiuser
-sudo sed -i -e '1i$IP_LIST' /etc/hosts
+sudo sed -i -e ./dependencies/node_list /etc/hosts
