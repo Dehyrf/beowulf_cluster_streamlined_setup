fe2749d13e646391d1027d23f1e73db91e852e96
input null <Dehyrf@users.noreply.github.com>
2015-12-03 11:57:25 -0600

    Update setup_part_2.sh

--- setup_part_2.sh
+++ setup_part_2.sh
@@ -12,14 +12,20 @@ read -p "Is this the master node?(Y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    #Share the home directory, restart the file server, and change firewall settings on the master node
-   sudo echo "/home/mpiuser *(rw,sync,no_subtree_check)" >> /etc/exports
-   sudo service nfs-kernel-server restart
-   read -e -p "Enter the subnet of the cluster: " -i "192.168.10.0" SUBN
-   sudo ufw allow from '$SUBN'/24
+        #Share the home directory, restart the file server, and change firewall settings on the master node
+        sudo echo "/home/mpiuser *(rw,sync,no_subtree_check)" >> /etc/exports
+        sudo service nfs-kernel-server restart
+        read -e -p "Enter the subnet of the cluster: " -i "192.168.10.0" SUBN
+        sudo ufw allow from '$SUBN'/24
+        
+        #Generate the master ssh key for all slave nodes and share to all using the NFS server
+        echo When asked for a passphrase, leave it empty! 
+        ssh-keygen
+        ssh-copy-id localhost
+        cd ~
+        touch hosts
+        while read host _; do echo "$host" >> ./dependencies/hosts "$word"; done < ./dependencies/node_list
 fi
 
 
-#Generate SSH Key
-ssh-keygen -t dsa
 
