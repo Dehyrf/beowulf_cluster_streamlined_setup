f83125a418f60d501caf62cea264f72d4270f1b1
input null <Dehyrf@users.noreply.github.com>
2015-12-03 10:43:01 -0600

    Create setup_part_1.sh

---
+++ setup_part_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Sets IP so it can change
+read -rsp $'Please begin without ethernet plugged in. Press any key to continue...\n' -n1 key
+bash ./dependencies/set_ip_volatile.sh 
+
+#Sets IP Static on the network and adds computer info to the node list file
+read -rsp $'Now plug in ethernet. Press any key to continue once it is connected...\n' -n1 key
+bash ./dependencies/set_current_ip_static.sh
+
+#Updates and installs programs needed
+sudo apt-get update
+sudo --yes install build-essentials openssh-server
+sudo --yes apt-get autoremove && sudo apt-get --yes autoclean
+
+#Creates a dedicated user
+sudo useradd mpiuser sudo
+echo Just made user: mpiuser
+sudo passwd mpiuser
