b602eea76a3a242af6138a8de26541d9dcaf8d0c
input null <Dehyrf@users.noreply.github.com>
2015-12-03 10:41:56 -0600

    Edits to make it part 1/2

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "Please begin without ethernet plugged in"
+#Sets IP so it can change
+read -rsp $'Please begin without ethernet plugged in. Press any key to continue...\n' -n1 key
 bash ./dependencies/set_ip_volatile.sh 
-echo "Now plug in ethernet. Does the computer show that it is connected? "
+
+#Sets IP Static on the network and adds computer info to the node list file
+read -rsp $'Now plug in ethernet. Press any key to continue once it is connected...\n' -n1 key
 bash ./dependencies/set_current_ip_static.sh
+
+#Updates and installs programs needed
 sudo apt-get update
 sudo --yes install build-essentials openssh-server
 sudo --yes apt-get autoremove && sudo apt-get --yes autoclean
+
+#Creates a dedicated user
 sudo useradd mpiuser sudo
+echo Just made user: mpiuser
 sudo passwd mpiuser
-sudo sed -i -e ./dependencies/node_list /etc/hosts
