92b5f0f7224456c4d8686e481155d60127c4b577
Dehyrf <Dehyrf@users.noreply.github.com>
2015-12-11 20:34:37 -0600

    Finished part 1

--- setup_part_1.sh
+++ setup_part_1.sh
@@ -7,8 +7,9 @@ bash ./dependencies/set_ip_volatile.sh
 #Updates and installs programs needed
 read -rsp $'Please connect to a network with internet access to download software (Note: this does not have to be your cluster network). Press any key to continue...\n' -n1 key
 sudo apt-get update
-sudo install build-essentials openssh-server nfs-kernel-server nfs-common
+sudo apt-get install build-essential openssh-server nfs-kernel-server nfs-common
 sudo dpkg -i ./dependencies/mpich2_1.4.1-1ubuntu1_i386.deb
+sudo apt-get -f install
 sudo apt-get autoremove && sudo apt-get autoclean
 
 #Sets IP Static on the network and adds computer info to the node list file
@@ -16,8 +17,9 @@ read -rsp $'Now, plug in ethernet to your gateway for the cluster. Press any key
 bash ./dependencies/set_current_ip_static.sh
 
 #Creates a dedicated user for the cluster, the accounts have no password (but you can change that if security is a issue with 'passwd mpiuser' on all nodes)
+echo Creating user mpiuser, please leave all fields blank/default!
 sudo adduser mpiuser --uid 999
 echo Just made user: mpiuser
 
 #Provides further instruction
-echo Now, please run the setup_part_1 script on all machines in the cluster. Once done, proceed to running setup_part_2 on the machines (but run it on the master node last).
+echo 'Now, please run the setup_part_1 script on all machines in the cluster. Once done, proceed to running setup_part_2 on the machines (but run it on the master node last).'
