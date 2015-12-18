e1375ea05ddc949cee0ca3e8e02dd50446923846
Dehyrf <Dehyrf@users.noreply.github.com>
2015-12-18 14:40:15 -0600

    Added progress indicator

--- setup_part_1.sh
+++ setup_part_1.sh
@@ -4,6 +4,10 @@
 read -rsp $'Please begin without a network connection of any kind. Press any key to continue...\n' -n1 key
 bash ./dependencies/set_ip_volatile.sh 
 
+echo
+echo 'Reset ip, finished with part 1 of 4'
+echo
+
 #Updates and installs programs needed
 read -rsp $'Please connect to a network with internet access to download software (Note: this does not have to be your cluster network). Press any key to continue...\n' -n1 key
 sudo apt-get update
@@ -12,14 +16,26 @@ sudo dpkg -i ./dependencies/mpich2_1.4.1-1ubuntu1_i386.deb
 sudo apt-get -f install
 sudo apt-get autoremove && sudo apt-get autoclean
 
+echo
+echo 'Installed needed programs, finished with part 2 of 4'
+echo
+
 #Sets IP Static on the network and adds computer info to the node list file
 read -rsp $'Now, plug in ethernet to your gateway for the cluster. Press any key to continue once it is connected...\n' -n1 key
 bash ./dependencies/set_current_ip_static.sh
+
+echo
+echo 'Set static ip, finished with part 3 of 4'
+echo
 
 #Creates a dedicated user for the cluster, the accounts have no password (but you can change that if security is a issue with 'passwd mpiuser' on all nodes)
 echo Creating user mpiuser, please leave all fields blank/default!
 sudo adduser mpiuser --uid 999
 echo Just made user: mpiuser
+
+echo
+echo 'Created user, finished with part 4 of 4'
+echo
 
 #Provides further instruction
 echo 'Now, please run the setup_part_1 script on all machines in the cluster. Once done, proceed to running setup_part_2 on the machines (but run it on the master node last).'
--- setup_part_2.sh
+++ setup_part_2.sh
@@ -13,29 +13,50 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then    
         #Remove the NFS client from the master
         sudo apt-get remove nfs-common
-        
+       
+	echo
+	echo 'Removed NFS client, finished with part 1 of 5'
+	echo
+ 
         #Share the home directory, restart the file server, and change firewall settings on the master node
         sudo echo "/home/mpiuser *(rw,sync,no_subtree_check)" >> /etc/exports
         sudo service nfs-kernel-server restart
         read -e -p "Enter the subnet of the cluster: " -i "192.168.10.0" SUBN
         sudo ufw allow from '$SUBN'/24
         
+	echo
+	echo 'Set up NFS server and firewall, finished with part 2 of 5'
+	echo
+
         #Generate the master ssh key for all slave nodes and share to all using the NFS server
         echo When asked for a passphrase, leave it empty! 
         ssh-keygen
         ssh-copy-id localhost
-        
+
+	echo
+	echo 'Generated ssh key, finished with part 3 of 5'
+	echo
+
         #Set up MPI and Hyrda
         cp ./dependencies/hosts ~/hosts
         cp ./dependencies/hosts ~/mpd.hosts
         cp ./dependencies/hosts ~/.mpd.conf
         chmod 600 ~/.mpd.conf
-        
+       
+	echo
+	echo 'Set up MPICH, finished with part 4 of 5'
+	echo
+ 
         #Set secret passphrase for the cluster
         read -e -p "Enter the secret passphrase for the cluster: " -i "Pa$$w0rd" SECRET
         echo "secretword="$SECRET"" >> ~/.mpd.conf
         
-        #Now, to boot MPI on the nodes
+	echo
+	echo 'Made passphrase, finished with part 5 of 5'
+	echo 'Now ready to boot nodes!'
+	echo
+
+        #Boot MPI on the nodes
         read -e -p "Number of slave nodes in the cluster: " -i "3" NODES
         mpdboot -n $NODES
         echo Below are the working nodes ready to go!
